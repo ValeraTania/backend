dfe3d3ef33080126b7b1dfe9458ae98b0dc3a761
Alunos <alunos@MacBook-Air-de-EDIT.local>
2025-03-26 22:49:15 +0000

    Ex endpoint

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ const msg: string = 'Hi, my first backend project';
 console.log(msg);
 
 const app: Express = express();
-const port: number = 4000;
+app.use(express.json());
+
+const port: number = 4001;
 let users: IUser[] = [
   { id: 0, name: 'Alex' },
   { id: 1, name: 'Tania' },
@@ -30,5 +32,24 @@ app.get('/users', (req: Request, res: Response) => {
 app.get('/users/:id', (req: Request, res: Response) => {
   const userId = parseInt(req.params.id);
   const foundUser = users.find((user) => user.id === userId);
+
+  if (!foundUser) {
+    res.status(404).send('User not found');
+  }
   res.json(foundUser);
+});
+
+app.post('/users', (req: Request, resp: Response) => {
+  //   const name = req.body.name;
+  //   const age = req.body.age;
+  const { name, age } = req.body;
+
+  const newUser: IUser = {
+    id: 123,
+    name: name,
+  };
+  
+  users.push(newUser);
+  console.log(name, age);
+  resp.status(201).send(newUser);
 });
